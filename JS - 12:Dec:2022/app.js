77a6705a2664d534c4e9623407d8206b60ee0cd9
Vinu's MacBook Pro <vinitvijal@Vinuss-MacBook-Pro.local>
2022-12-12 20:12:01 +0530

    Day 1

---
+++ JS - 12:Dec:2022/app.js
@@ -0,0 +1,42 @@
+// // Identifier
+
+// var num1 = 6;
+// var num2 = 7;
+
+// var name = "Ram";
+
+// // Print
+// // console.log('Hi '+ name +', You have passed your exam!, You have got '+num1+' CGPA')
+
+// console.log(`Oye ${name}, You have passed your exam!, You have got ${num1} CGPA`)
+
+
+
+
+
+
+
+
+// var a = 15;
+
+// if (a % 5 == 0){
+//     console.log('Yes I am multiple of 5')
+// }
+// if(a % 3 == 0){
+//     console.log('Yes I am multiple of 3')
+// }else{
+//     console.log('I am a bad number')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
